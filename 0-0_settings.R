c4d86d4d8336e61108577e6be632cc1a5c7a6456
Lorenz <51027264+lorenz221092@users.noreply.github.com>
2022-02-09 16:16:53 +0100

    Add files via upload

---
+++ 0-0_settings.R
@@ -0,0 +1,20 @@
+# script:  0-0_settings.R
+# purpose: load packages and settings
+# project: predicting input coefficients
+# author:  lorenz wimmer
+##########################################################################################
+##########################################################################################
+
+require(tidyverse)
+require(readxl)
+require(foreign)
+require(fastDummies)
+require(eurostat)
+require(countrycode)
+require(openxlsx)
+
+
+
+
+##################end of script###########################################################
+##########################################################################################
